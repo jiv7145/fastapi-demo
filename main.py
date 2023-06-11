dd518e48398b3af5800ff91aa0b12065bb8c1c35
Jake <jiv7145@gmail.com>
2023-06-11 02:11:13 -0700

    8

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from starlette.responses import FileResponse
+from uvicorn import run
 
 app = FastAPI()
 
@@ -11,3 +12,6 @@ async def root():
 @app.get("/dl")
 async def dl():
     return FileResponse('./txt_file.txt', filename='my_txt_file.txt')
+
+if __name__ == "__main__":
+    run(app, host="0.0.0.0", port=8080)
